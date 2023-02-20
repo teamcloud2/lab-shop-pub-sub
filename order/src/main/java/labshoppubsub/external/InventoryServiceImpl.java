2b0a6e7a3f5ca77deed7cfce55c4d8733de5ab34
A R <85432029+teamcloud2@users.noreply.github.com>
2023-02-20 16:48:24 +0900

    commit

---
+++ order/src/main/java/labshoppubsub/external/InventoryServiceImpl.java
@@ -0,0 +1,16 @@
+package labshoppubsub.external;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryServiceImpl implements InventoryService {
+
+
+    /**
+     * Fallback
+     */
+    public Inventory getInventory(Long id) {
+        Inventory inventory = new Inventory();
+        return inventory;
+    }
+}
